908944d54867f59d7f8786d1a6eec8409b3d1d9c
Siddalinga <siddaling.gkoudi@gmail.com>
2022-02-23 21:15:13 +0530

    Added a Procfile.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 const express = require("express")
 require("dotenv").config()
 const connect = require("./src/configs/db")
