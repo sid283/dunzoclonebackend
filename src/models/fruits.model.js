4193bfcb9e240e27bf7f05b8521ae2348f5cab2a
Siddalinga <siddaling.gkoudi@gmail.com>
2022-02-23 20:39:48 +0530

    Added a Procfile.

--- src/models/fruits.model.js
+++ src/models/fruits.model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const fruitSchema = new mongoose.Schema({
-    id:{type:Number, required: true,unique:true},
+    id:{type:String, required: true,unique:true},
     title:{type:String, required: true},
-    price:{type:Number, required: true},
+    price:{type:String, required: true},
     image:{type:String, required: true}
 },
 {
